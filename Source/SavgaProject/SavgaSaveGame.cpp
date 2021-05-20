4f66ce02966ae7a053f1fd3a921ce176365ef876
Çağatay Sarıoğlu <sarioglucagatay@gmail.com>
2021-05-20 19:27:30 +0300

    ...

---
+++ Source/SavgaProject/SavgaSaveGame.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SavgaSaveGame.h"
+
+USavgaSaveGame::USavgaSaveGame()
+{
+	PlayerLocation = FVector(0.f, 0.f, 0.f);
+}
